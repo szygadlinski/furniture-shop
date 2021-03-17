1f74525dbc870076c9d5d70fbbf9aa12104f3422
Dorota Tomala <doforever@gmail.com>
2021-03-17 15:49:25 +0100

    Recognize swipe direction for mouse and touch events

--- src/components/common/Swipeable/Swipeable.js
+++ src/components/common/Swipeable/Swipeable.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Swipeable = ({ leftAction, rightAction, children }) => {
-  return <div>{children}</div>;
+  const [touchStart, setTouchStart] = useState(null);
+
+  const handleTouchStart = event => {
+    setTouchStart(event.clientX);
+  };
+
+  const handleTouchEnd = event => {
+    if (touchStart - event.clientX > 150) {
+      // left swipe
+      console.log('left swipe');
+    }
+
+    if (touchStart - event.clientX < -150) {
+      //  right swipe
+      console.log('right swipe');
+    }
+  };
+
+  return (
+    <div
+      onTouchStart={event => handleTouchStart(event)}
+      onTouchEnd={event => handleTouchEnd(event)}
+      onMouseDown={event => handleTouchStart(event)}
+      onMouseUp={event => handleTouchEnd(event)}
+    >
+      {children}
+    </div>
+  );
 };
 
 Swipeable.propTypes = {
