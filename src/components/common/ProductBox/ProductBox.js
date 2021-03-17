7b6caf50ea5d06723762e9f1d7b41238ea143f25
Dorota Tomala <doforever@gmail.com>
2021-03-17 11:04:37 +0100

    Add oldPrice prop to ProductBox

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,7 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-const ProductBox = ({ name, price, promo, stars }) => (
+const ProductBox = ({ name, price, oldPrice, promo, stars }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -47,9 +47,11 @@ const ProductBox = ({ name, price, promo, stars }) => (
         </Button>
       </div>
       <div className={styles.price}>
-        <Button className={styles.old_price} noHover variant='small'>
-          $ 40
-        </Button>
+        {oldPrice && (
+          <Button className={styles.old_price} noHover variant='small'>
+            $ {oldPrice}
+          </Button>
+        )}
         <Button noHover variant='small'>
           $ {price}
         </Button>
@@ -62,6 +64,7 @@ ProductBox.propTypes = {
   children: PropTypes.node,
   name: PropTypes.string,
   price: PropTypes.number,
+  oldPrice: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
 };
