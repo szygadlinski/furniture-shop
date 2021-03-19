06e481aaeaf36fd6b1b714ff20def675b0372c15
Karolina Okla <karolinaokla95@gmail.com>
2021-03-19 21:34:18 +0100

    Add RWD to Menu bar

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -7,7 +7,7 @@ import styles from './MenuBar.module.scss';
 
 const MenuBar = ({ children }) => (
   <div className={styles.root}>
-    <div className='container'>
+    <div className={'container ' + styles.normalMenu}>
       <div className='row align-items-center'>
         <div className='col'>
           <ProductSearch />
@@ -39,6 +39,44 @@ const MenuBar = ({ children }) => (
             </li>
           </ul>
         </div>
+      </div>
+    </div>
+
+    <div className={styles.dropdownMenu}>
+      <div className={styles.search}>
+        <ProductSearch />
+      </div>
+      <div className={styles.droplistMenu}>
+        <ul className={styles.dropMenu}>
+          <li className={styles.dropFirst}>
+            <a className={styles.menuLink} href='#'>
+              Menu
+            </a>
+            <ul className={styles.dropdown}>
+              <li>
+                <a href='#'>Home</a>
+              </li>
+              <li>
+                <a href='#'>Furniture</a>
+              </li>
+              <li>
+                <a href='#'>Chair</a>
+              </li>
+              <li>
+                <a href='#'>Table</a>
+              </li>
+              <li>
+                <a href='#'>Sofa</a>
+              </li>
+              <li>
+                <a href='#'>Bedroom</a>
+              </li>
+              <li>
+                <a href='#'>Blog</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
