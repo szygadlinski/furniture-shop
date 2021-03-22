99827a5b44ddfdc3f396ab8dc076a2e6d5e5b109
Karolina Okla <karolinaokla95@gmail.com>
2021-03-22 14:07:25 +0100

    Add NewFurniture do product page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MainLayout from './components/layout/MainLayout/MainLayoutContainer.js';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
+import Products from './components/views/Products/Products';
 
 const App = () => (
   <Provider store={store}>
@@ -18,6 +19,7 @@ const App = () => (
         <Switch>
           <Route exact path={'/'} component={Homepage} />
           <Route exact path={'/shop/:categoryId'} component={ProductList} />
+          <Route exact path={'/product'} component={Products} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
         </Switch>
       </MainLayout>
