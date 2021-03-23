21707544f9bd6aab746f3153a719c0a189c28fe6
Dorota Tomala <doforever@gmail.com>
2021-03-23 14:17:26 +0100

    Fix adding class main by default

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -9,7 +9,7 @@ const Button = ({ children, variant, noHover, className: propClassName, ...props
   if (propClassName) classes.push(propClassName);
 
   if (variant) classes.push(styles[variant]);
-  else classes.push('main');
+  else classes.push(styles.main);
 
   let Comp = 'a';
 
