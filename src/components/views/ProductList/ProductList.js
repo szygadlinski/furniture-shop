0e30622046c0c3387f9585189941af135d66d081
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-03-25 22:21:22 +0100

    Implement ColorFilter onto subpage

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
+import ColorFilter from '../../features/ColorFilter/ColorFilter';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => (
+  <div>
+    <ColorFilter />
+    <div className={styles.root}>This is ProductList</div>
+  </div>
+);
 
 // ProductList.propTypes = {};
 
