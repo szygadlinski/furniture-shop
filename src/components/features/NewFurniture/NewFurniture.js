519c9e716a526cf675c7a2d44a89c23156f44bcf
Karolina Okla <karolinaokla95@gmail.com>
2021-03-26 17:01:16 +0100

    Add image prop to NewFurniture

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -119,7 +119,7 @@ class NewFurniture extends React.Component {
           >
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
               <div key={item.id} className='col-6 col-md-4 col-lg-3'>
-                <ProductBox {...item} />
+                <ProductBox {...item} image={image} />
               </div>
             ))}
           </div>
