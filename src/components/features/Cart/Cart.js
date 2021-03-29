f3ce69c53dc269be65ca5f747f36c47b0828158c
Dorota Tomala <doforever@gmail.com>
2021-03-29 09:48:05 +0200

    Add react-router-dom Link to Cart

--- src/components/features/Cart/Cart.js
+++ src/components/features/Cart/Cart.js
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 
 import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 import styles from './Cart.module.scss';
 
 const Cart = ({ count }) => (
-  <a className={styles.cart}>
+  <Link to='/cart' className={styles.cart}>
     <div className={styles.cartIcon}>
       <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
     </div>
     <div className={styles.cartCounter}>{count}</div>
-  </a>
+  </Link>
 );
 
 Cart.propTypes = {
