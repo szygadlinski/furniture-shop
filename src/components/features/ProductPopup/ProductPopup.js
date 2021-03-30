182226f3de8ae7f8dae9573d790be897a4be9db6
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-03-30 09:29:26 +0200

    Checking git

--- src/components/features/ProductPopup/ProductPopup.js
+++ src/components/features/ProductPopup/ProductPopup.js
@@ -9,7 +9,12 @@ const ProductPopup = () => {
         <span className='close-icon'>x</span>
         <h1>Product details</h1>
         <div className={styles.box}>
-          <div className={styles.picture}></div>
+          <div className={styles.picture}>
+            <img
+              src='https://cdn.pixabay.com/photo/2014/07/10/17/17/bedroom-389254_1280.jpg'
+              alt='yes'
+            />
+          </div>
           <div className={styles.details}></div>
         </div>
       </div>
