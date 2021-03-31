51591b8721ec5de9e39c7e188520eb1c406cbf4b
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-03-31 13:48:57 +0200

    Add language.js

---
+++ src/language.js
@@ -0,0 +1,365 @@
+export const EN = {
+  language: 'English',
+
+  Banner: {
+    TITLE_TEXT: 'BEDROOM',
+    TITLE_SPAN_TEXT: 'FURNITURE',
+    SUBTITLE_PRESPAN_TEXT: 'ALWAYS',
+    SUBTITLE_POSTSPAN_TEXT: 'OFF OR MORE',
+    HOME_LINK_TEXT: 'Home',
+    FURNITURE_LINK_TEXT: 'Furniture',
+  },
+
+  BlogBox: {
+    TITLE_TEXT: 'Welcome to the new store website!',
+    DATE_TEXT: 'June 16, 2020',
+    BUTTON_TEXT: 'Read more...',
+  },
+
+  CategoryFilter: {
+    TITLE_TEXT: 'FILTER BY CATEGORIES',
+    BUTTON_FURNITURE_TEXT: 'furniture',
+    BUTTON_SOFA_TEXT: 'sofa',
+    BUTTON_CHAIR_TEXT: 'chair',
+    BUTTON_TABLE_TEXT: 'table',
+    BUTTON_BEDROOM_TEXT: 'bedroom',
+    BUTTON_DINING_TEXT: 'dining',
+  },
+
+  ChatBox: {
+    MESSAGE_TEXT: 'How can I help you?',
+    BUTTON_TEXT: 'If you need help, write me!',
+  },
+
+  ColorFilter: {
+    TITLE_TEXT: 'FILTER BY COLOUR',
+    BUTTON_RED_TEXT: 'Red',
+    BUTTON_BLACK_TEXT: 'Black',
+    BUTTON_YELLOW_TEXT: 'Yellow',
+    BUTTON_BLUE_TEXT: 'Blue',
+    BUTTON_PINK_TEXT: 'Pink',
+    BUTTON_GREEN_TEXT: 'Green',
+  },
+
+  Deals: {
+    LEFT_TITLE_TEXT: 'Guest room',
+    LEFT_SUBTITLE_TEXT: 'Chairs',
+    RIGHT_UP_TITLE_TEXT: 'Bedroom',
+    RIGHT_UP_SUBTITLE_TEXT: 'Collection',
+    RIGHT_DOWN_TITLE_TEXT: 'Special collection',
+    RIGHT_DOWN_SUBTITLE_TEXT: 'Save up 45% of furniture',
+  },
+
+  FeatureBoxes: {
+    SHIPPING_TITLE_TEXT: 'Free shipping',
+    SHIPPING_SUBTITLE_TEXT: 'All orders',
+
+    SUPPORT_TITLE_TEXT: '24/7 customer',
+    SUPPORT_SUBTITLE_TEXT: 'support',
+
+    MONEY_TITLE_TEXT: 'Money back',
+    MONNEY_SUBTITLE_TEXT: 'guarantee',
+
+    MEMBER_TITLE_TEXT: 'Member discount',
+    MEMBER_SUBTITLE_TEXT: 'First order',
+  },
+
+  Feedback: {
+    TITLE_TEXT: 'Client Feedback',
+  },
+
+  Footer: {
+    INFO_TITLE_TEXT: 'Information',
+    LINK_ABOUT_TEXT: 'About us',
+    LINK_POLICY_TEXT: 'Policy',
+    LINK_CONDITIONS_TEXT: 'Conditions',
+    LINK_SUPPORT_TEXT: 'Online support',
+
+    ACCOUNT_TITLE_TEXT: 'My account',
+    LINK_LOGIN_TEXT: 'Login',
+    LINK_CART_TEXT: 'My cart',
+    LINK_WISHLIST_TEXT: 'Wishlist',
+    LINK_CHECKOUT_TEXT: 'Checkout',
+
+    INFO2_TITLE_TEXT: 'Information',
+    LINK_SPECIALS_TEXT: 'Specials',
+    LINK_NEWPRODUCTS_TEXT: 'New products',
+    LINK_BESTSELLERS_TEXT: 'Best sellers',
+    LINK_OUTSTORES_TEXT: 'Out stores',
+
+    ORDERS_TITLE_TEXT: 'Orders',
+    LINK_PAYMENT_TEXT: 'Payment options',
+    LINK_DELIVERY_TEXT: 'Shipping and delivery',
+    LINK_RETURNS_TEXT: 'Returns',
+    LINK_SHIPPING_TEXT: 'Shipping',
+  },
+
+  Gallery: {
+    TITLE_TEXT: 'Furniture Gallery',
+    CARD_FEATURED_TEXT: 'Featured',
+    CARD_TOPSELLER_TEXT: 'TopSeller',
+    CARD_SALEOFF_TEXT: 'Sale off',
+    CARD_TOPRATED_TEXT: 'Top rated',
+    PROMO_INNER_TEXT: 'from',
+    PRODUCT_NAME_TEXT: 'Bedroom Bed',
+    BUTTON_TEXT: 'Shop now',
+  },
+
+  GalleryProduct: {
+    BUTTON_FAVORITE_TEXT: 'Favorite',
+    BUTTON_COMPARE_TEXT: 'Add to compare',
+    BUTTON_QUICKVIEW_TEXT: 'Quick view',
+    BUTTON_ADDTOCART_TEXT: 'Add to cart',
+  },
+
+  MenuBar: {
+    CARD_HOME_TEXT: 'Home',
+    CARD_FURNITURE_TEXT: 'Furniture',
+    CARD_CHAIR_TEXT: 'Chair',
+    CARD_TABLE_TEXT: 'Table',
+    CARD_SOFA_TEXT: 'Sofa',
+    CARD_BEDROOM_TEXT: 'Bedroom',
+    CARD_BLOG_TEXT: 'Blog',
+  },
+
+  NewFurniture: {
+    TITLE_TEXT: 'New furniture',
+  },
+
+  PriceFilter: {
+    TITLE_TEXT: 'Filter by Price',
+    BUTTON_TEXT: 'FILTER',
+  },
+
+  ProductBox: {
+    BUTTON_QUICKVIEW_TEXT: 'Quick view',
+    BUTTON_ADDTOCART_TEXT: 'Add to cart',
+    BUTTON_FAVORITE_TEXT: 'Favorite',
+    BUTTON_COMPARE_TEXT: 'Add to compare',
+  },
+
+  ProductSearch: {
+    SELECT_TEXT: 'Select a category',
+    BUTTON_BED_TEXT: 'bed',
+    BUTTON_CHAIR_TEXT: 'chair',
+    BUTTON_SOFA_TEXT: 'sofa',
+    BUTTON_TABLE_TEXT: 'table',
+    BUTTON_DINING_TEXT: 'dining',
+  },
+
+  Promoted: {
+    TITLE_TEXT: 'HOT DEALS',
+    BUTTON_ADDTOCART_TEXT: 'ADD TO CART',
+    DOT_DAY_TEXT: 'DAYS',
+    DOT_HR_TEXT: 'HRS',
+    DOT_MIN_TEXT: 'MINS',
+    DOT_SEC_TEXT: 'SECS',
+
+    RIGHT_TITLE_TEXT: 'INDOOR',
+    RIGHT_TITLE2_TEXT: 'FURNITURE',
+    RIGHT_SUBTITLE_TEXT: 'save up to 50% of all furniture',
+    BUTTON_SHOP_TEXT: 'SHOP NOW',
+  },
+
+  SizeFilter: {
+    TITLE_TEXT: 'Filter by size',
+  },
+
+  StickyBar: {
+    BUTTON_TEXT: 'Compare',
+  },
+
+  TopBar: {
+    TITLE_LANGUAGE_TEXT: 'English',
+    BUTTON_ENGLISH_TEXT: 'English',
+    BUTTON_POLISH_TEXT: 'Polish',
+
+    TITLE_HELP_TEXT: 'Help',
+    BUTTON_PAYMENT_TEXT: 'Payment',
+    BUTTON_DELIVERY_TEXT: 'Delivery',
+    BUTTON_SHOPLIST_TEXT: 'Shop list',
+
+    BUTTON_LOGIN_TEXT: 'Login',
+    BUTTON_REGISTER_TEXT: 'Register',
+  },
+};
+
+export const PL = {
+  language: 'Polish',
+
+  Banner: {
+    TITLE_TEXT: 'Meble',
+    TITLE_SPAN_TEXT: 'Do sypialni',
+    SUBTITLE_PRESPAN_TEXT: 'Co najmniej',
+    SUBTITLE_POSTSPAN_TEXT: 'Taniej',
+    HOME_LINK_TEXT: 'Strona domowa',
+    FURNITURE_LINK_TEXT: 'Meble',
+  },
+
+  BLOGBOX: {
+    TITLE_TEXT: 'Witamy na nowej stronie sklepu!',
+    DATE_TEXT: '16 czerwca 2020',
+    BUTTON_TEXT: 'Czytaj więcej...',
+  },
+
+  CategoryFilter: {
+    TITLE_TEXT: 'Filtruj po kategorii',
+    BUTTON_FURNITURE_TEXT: 'Meble',
+    BUTTON_SOFA_TEXT: 'Sofy',
+    BUTTON_CHAIR_TEXT: 'Krzesła',
+    BUTTON_TABLE_TEXT: 'Stoły',
+    BUTTON_BEDROOM_TEXT: 'Łóżka',
+    BUTTON_DINING_TEXT: 'Do jadalni',
+  },
+
+  ChatBox: {
+    MESSAGE_TEXT: 'W czym mogę pomóc?',
+    BUTTON_TEXT: 'Potrzebujesz pomocy? Napisz!',
+  },
+
+  ColorFilter: {
+    TITLE_TEXT: 'Filtruj po kolorze',
+    BUTTON_RED_TEXT: 'Czerwony',
+    BUTTON_BLACK_TEXT: 'Czarny',
+    BUTTON_YELLOW_TEXT: 'Żółty',
+    BUTTON_BLUE_TEXT: 'Niebieski',
+    BUTTON_PINK_TEXT: 'Różowy',
+    BUTTON_GREEN_TEXT: 'Zielony',
+  },
+
+  Deals: {
+    LEFT_TITLE_TEXT: 'Krzesła do',
+    LEFT_SUBTITLE_TEXT: 'Pokoi gościnnych',
+    RIGHT_UP_TITLE_TEXT: 'Kolekcja',
+    RIGHT_UP_SUBTITLE_TEXT: 'Do sypialni',
+    RIGHT_DOWN_TITLE_TEXT: 'Kolekcja specjalna',
+    RIGHT_DOWN_SUBTITLE_TEXT: 'Zaoszczędź 45% ceny mebli',
+  },
+
+  FeatureBoxes: {
+    SHIPPING_TITLE_TEXT: 'Darmowa dostawa',
+    SHIPPING_SUBTITLE_TEXT: 'Wszystkich zamówień',
+    SUPPORT_TITLE_TEXT: '24/7 wsparcie',
+    SUPPORT_SUBTITLE_TEXT: 'Klienta',
+    MONEY_TITLE_TEXT: 'Zwrot gotówki',
+    MONNEY_SUBTITLE_TEXT: 'Gwarantowany',
+    MEMBER_TITLE_TEXT: 'Rabat dla członków',
+    MEMBER_SUBTITLE_TEXT: 'Przy pierwszym zamówieniu',
+  },
+
+  Feedback: {
+    TITLE_TEXT: 'Opinie klientów',
+  },
+
+  Footer: {
+    INFO_TITLE_TEXT: 'Informacje',
+    LINK_ABOUT_TEXT: 'O nas',
+    LINK_POLICY_TEXT: 'Polityka prywatności',
+    LINK_CONDITIONS_TEXT: 'Regulamin',
+    LINK_SUPPORT_TEXT: 'Wsparcie online',
+
+    ACCOUNT_TITLE_TEXT: 'Moje konto',
+    LINK_LOGIN_TEXT: 'Logowanie',
+    LINK_CART_TEXT: 'Mój koszyk',
+    LINK_WISHLIST_TEXT: 'Lista życzeń',
+    LINK_CHECKOUT_TEXT: 'Sprawdź zamówienie',
+
+    INFO2_TITLE_TEXT: 'Informacje',
+    LINK_SPECIALS_TEXT: 'Specialności',
+    LINK_NEWPRODUCTS_TEXT: 'Nowe produkty',
+    LINK_BESTSELLERS_TEXT: 'Bestsellery',
+    LINK_OUTSTORES_TEXT: 'Nasze sklepy',
+
+    ORDERS_TITLE_TEXT: 'Zamówienia',
+    LINK_PAYMENT_TEXT: 'Formy płatności',
+    LINK_DELIVERY_TEXT: 'Dostawy',
+    LINK_RETURNS_TEXT: 'Zwroty',
+    LINK_SHIPPING_TEXT: 'Wysyłka',
+  },
+
+  Gallery: {
+    TITLE_TEXT: 'Galeria mebli',
+    CARD_FEATURED_TEXT: 'Wyróżnione',
+    CARD_TOPSELLER_TEXT: 'Topsellery',
+    CARD_SALEOFF_TEXT: 'Wyprzedaże',
+    CARD_TOPRATED_TEXT: 'Najwyżej oceniane',
+    PROMO_INNER_TEXT: 'Od',
+    PRODUCT_NAME_TEXT: 'Łóżko do sypialni',
+    BUTTON_TEXT: 'Kup teraz',
+  },
+
+  GalleryProduct: {
+    BUTTON_FAVORITE_TEXT: 'Do ulubionych',
+    BUTTON_COMPARE_TEXT: 'Do porównania',
+    BUTTON_QUICKVIEW_TEXT: 'Szybki podgląd',
+    BUTTON_ADDTOCART_TEXT: 'Do koszyka',
+  },
+
+  MenuBar: {
+    CARD_HOME_TEXT: 'Strona domowa',
+    CARD_FURNITURE_TEXT: 'Meble',
+    CARD_CHAIR_TEXT: 'Krzesła',
+    CARD_TABLE_TEXT: 'Stoły',
+    CARD_SOFA_TEXT: 'Sofy',
+    CARD_BEDROOM_TEXT: 'Łóżka',
+    CARD_BLOG_TEXT: 'Blog',
+  },
+
+  NewFurniture: {
+    TITLE_TEXT: 'Nowości',
+  },
+
+  PriceFilter: {
+    TITLE_TEXT: 'Filtruj po cenie',
+    BUTTON_TEXT: 'Filtruj',
+  },
+
+  ProductBox: {
+    BUTTON_QUICKVIEW_TEXT: 'Szybki podględ',
+    BUTTON_ADDTOCART_TEXT: 'Do koszyka',
+    BUTTON_FAVORITE_TEXT: 'Do ulubionych',
+    BUTTON_COMPARE_TEXT: 'Do porównania',
+  },
+
+  ProductSearch: {
+    SELECT_TEXT: 'Wybierz kategorię',
+    BUTTON_BED_TEXT: 'Łóżka',
+    BUTTON_CHAIR_TEXT: 'Krzesła',
+    BUTTON_SOFA_TEXT: 'Sofy',
+    BUTTON_TABLE_TEXT: 'Stoły',
+    BUTTON_DINING_TEXT: 'Do jadalni',
+  },
+
+  Promoted: {
+    TITLE_TEXT: 'Gorące okazje',
+    BUTTON_ADDTOCART_TEXT: 'Do koszyka',
+    DOT_DAY_TEXT: 'Dni',
+    DOT_HR_TEXT: 'Godzin',
+    DOT_MIN_TEXT: 'Minut',
+    DOT_SEC_TEXT: 'Sekund',
+    RIGHT_TITLE_TEXT: 'Meble',
+    RIGHT_TITLE2_TEXT: 'Wewnętrzne',
+    RIGHT_SUBTITLE_TEXT: 'Oszczędź do 50% na wszytkich meblach',
+    BUTTON_SHOP_TEXT: 'Kup teraz',
+  },
+
+  SizeFilter: {
+    TITLE_TEXT: 'Filtruj po rozmiarze',
+  },
+
+  StickyBar: {
+    BUTTON_TEXT: 'Porówniaj',
+  },
+
+  TopBar: {
+    TITLE_LANGUAGE_TEXT: 'Polski',
+    BUTTON_ENGLISH_TEXT: 'Angielski',
+    BUTTON_POLISH_TEXT: 'Polski',
+    TITLE_HELP_TEXT: 'Pomoc',
+    BUTTON_PAYMENT_TEXT: 'Płatność',
+    BUTTON_DELIVERY_TEXT: 'Dostawa',
+    BUTTON_SHOPLIST_TEXT: 'Sklepy',
+    BUTTON_LOGIN_TEXT: 'Logowanie',
+    BUTTON_REGISTER_TEXT: 'Rejestracja',
+  },
+};
