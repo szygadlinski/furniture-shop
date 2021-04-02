7bf522eaa067b019a1177cf82c46e29ff4c3b7a5
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-04-02 15:30:31 +0200

    Add new layout

--- src/components/views/Register/Register.js
+++ src/components/views/Register/Register.js
@@ -1,59 +1,117 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import styles from './Register.module.scss';
 import Button from '../../common/Button/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ valid = false }) => {
+  const validate = event => {
+    event.preventDefault();
+    const firstName = document.getElementById('first_name').value;
+    const lastName = document.getElementById('last_name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmedPassword = document.getElementById('confirmPassword').value;
+    const termsAndConditions = document.getElementById('conditions').checked;
+    console.log(email.includes('@'));
+    //if(
+    //   (firstName.length >= 3 && firstName.length <= 30)
+    //  && (lastName.length >=3 && lastName.length <= 30)
+    //   && email.contains()
+    // ) return true;
+  };
+
   return (
     <div className={styles.root}>
       <div className='container'>
-        <div className={styles.containerWrapper}>
-          <form className={styles.radioForm}>
-            <div className={styles.radioBox}>
-              <Button>
-                <a href='/login'>I have an account</a>
-              </Button>
+        <h2 className={styles.title}>Create your account</h2>
+        <form className={styles.loginWrapper}>
+          <div>
+            <input
+              type='text'
+              id='first_name'
+              minLength='3'
+              maxLength='30'
+              name='first_name'
+              required
+              placeholder=' '
+            />
+            <label htmlFor='first_name'>First Name</label>
+            <div className={styles.requirements}>
+              Your first name must be at least 3 characters and max 30 characters.
             </div>
-          </form>
-
-          <div className={styles.titleWrapper}>
-            <h5>Create account</h5>
           </div>
 
-          <form className={styles.registerWrapper}></form>
+          <div>
+            <input
+              type='text'
+              id='last_name'
+              minLength='3'
+              maxLength='30'
+              name='last_name'
+              required
+              placeholder=' '
+            />
+            <label htmlFor='last_name'>Last Name</label>
+            <div className={styles.requirements}>
+              Your last name must be at least 3 characters and max 30 characters.
+            </div>
+          </div>
 
+          <div>
+            <input type='email' id='email' name='email' required placeholder=' ' />
+            <label htmlFor='email'>Email Address</label>
+            <div className={styles.requirements}>Must be a valid email address.</div>
+          </div>
+
+          <div>
+            <input
+              type='password'
+              id='password'
+              minLength='3'
+              name='password'
+              required
+              placeholder=' '
+            />
+            <label htmlFor='password'>Password</label>
+            <div className={styles.requirements}>
+              Your password must be at least 3 characters.
+            </div>
+          </div>
+
+          <div>
+            <input
+              type='password'
+              id='confirmPassword'
+              minLength='3'
+              name='confirmPassword'
+              required
+              placeholder=' '
+            />
+            <label htmlFor='confirmPassword'>Confirm password</label>
+            <div className={styles.requirements}>
+              Your password must be at least 3 characters.
+            </div>
+          </div>
           <form className={styles.checkboxWrapper}>
             <div className={styles.conditions}>
-              <input type='checkbox' />
-              <h6 className={styles.checkAll}>Select all</h6>
-            </div>
-            <div className={styles.conditions}>
-              <input type='checkbox' />
+              <input type='checkbox' id='conditions' />
               <h6>
                 I accept <a href='/#'>Terms and Conditions</a> *
               </h6>
             </div>
-            <div className={styles.conditions}>
-              <input type='checkbox' />
-              <h6>Sign up for newsletter</h6>
-            </div>
           </form>
 
-          <div className={styles.buttonsWrapper}>
-            <Button variant='outline' className={styles.btn1}>
-              &#10094; Cancel
-            </Button>
-            <Button variant='outline' className={styles.btn2}>
-              Sign up
-            </Button>
-          </div>
-        </div>
+          <Button className={styles.button} onClick={validate}>
+            <a href='/'>Sign up</a>
+          </Button>
+        </form>
       </div>
     </div>
   );
 };
-// Register.propTypes = {};
+
+Register.propTypes = {
+  valid: PropTypes.bool,
+};
 
 export default Register;
